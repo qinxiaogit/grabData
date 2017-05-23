0390b3f22be8fd2b2f0e2dbd261367e5493cc8e7
DESKTOP-1SA9GJQ\newugo01 <qinxiaolove@gmail.com>
2017-05-23 18:34:42 +0800

    环境切换

--- tests/ExampleSeleniumTest.php
+++ tests/ExampleSeleniumTest.php
@@ -8,14 +8,25 @@ class ExampleSeleniumTest extends PHPUnit_Extensions_Selenium2TestCase
     {
         $this->setBrowser(env('BROWSER'));
       //  $this->setBrowserUrl('http://' . env('WEBSERVER_URL') . ':' . env('WEBSERVER_PORT'));
-        $this->setBrowserUrl('https://www.baidu.com' );
+        $this->setBrowserUrl('https://dev.newugo.cn' );
         $this->setHost(env('SELENIUM_URL'));
         $this->setPort((int)env('SELENIUM_PORT'));
     }
 
     public function testTitle()
     {
-        $this->url('/');
-        $this->assertEquals('百度一下，你就知道', $this->title());
+        $this->url('/panel/login');
+        $this->element('css selector', 'input[name=login-username]')->value(array('value' => str_split('fitone')));
+        $this->element('css selector', 'input[name=login-password]')->value(array('value' => str_split('getgoodgo')));
+        //模拟点击登入按钮
+        $elements = $this->element('css selector', '.c-btn-oran-big')->click();
+
+        //打开 m.taobao.com，此时已获取到cookie
+        $this->open('dev.newugo.cn/panel/club');
+
+        //等待ajax加载完毕
+        waitForAjax();
+        $elements = $this->element('css selector', 'body')->text();
+        var_dump($elements);
     }
 }
